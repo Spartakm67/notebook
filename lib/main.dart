82533280636851122b8641063f7c008633d31e17
Spartakm67 <spartakm@ukr.net>
2025-05-22 23:46:49 +0300

    Cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notebook/theme/app_theme.dart';
@@ -60,79 +59,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyAppState extends ChangeNotifier {
-//   final favoritesBox = Hive.box<WordPairWrapper>('favoritesBox');
-//   final historyBox = Hive.box<WordPairWrapper>('historyBox');
-//
-//   WordPair current = WordPair.random();
-//   bool isDarkMode = ThemeService.getTheme();
-//
-//   List<WordPair> favorites = [];
-//   List<WordPair> history = [];
-//
-//   GlobalKey<AnimatedListState>? historyListKey;
-//
-//   MyAppState() {
-//     _loadData();
-//   }
-//
-//   void _loadData() {
-//     favorites = favoritesBox.values.map((wp) => wp.toWordPair()).toList();
-//     history = historyBox.values.map((wp) => wp.toWordPair()).toList();
-//   }
-//
-//   void getNext() {
-//     addToHistory(current);
-//     current = WordPair.random();
-//     notifyListeners();
-//   }
-//
-//   void toggleTheme() {
-//     isDarkMode = !isDarkMode;
-//     ThemeService.saveTheme(isDarkMode);
-//     notifyListeners();
-//   }
-//
-//   void toggleFavorite([WordPair? pair]) {
-//     pair = pair ?? current;
-//     if (favorites.contains(pair)) {
-//       removeFavorite(pair);
-//     } else {
-//       favorites.add(pair);
-//       favoritesBox.add(WordPairWrapper.fromWordPair(pair));
-//       notifyListeners();
-//     }
-//   }
-//
-//   void removeFavorite(WordPair pair) {
-//     favorites.remove(pair);
-//
-//     final keysToRemove = favoritesBox.keys.where((k) {
-//       final saved = favoritesBox.get(k);
-//       return saved?.toWordPair() == pair;
-//     }).toList();
-//
-//     for (var key in keysToRemove) {
-//       favoritesBox.delete(key);
-//     }
-//
-//     notifyListeners();
-//   }
-//
-//
-//   void addToHistory(WordPair pair) {
-//     history.insert(0, pair);
-//     historyBox.add(WordPairWrapper.fromWordPair(pair));
-//     historyListKey?.currentState?.insertItem(0);
-//     notifyListeners();
-//   }
-//
-//   void clearHistory() {
-//     history.clear();
-//     historyBox.clear();
-//     notifyListeners();
-//   }
-// }
-
 
--- lib/presentation/widgets/big_card.dart
+++ lib/presentation/widgets/big_card.dart
@@ -1,10 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:notebook/theme/app_theme.dart';
-import 'package:notebook/theme/theme_service.dart';
 import 'package:notebook/theme/app_text_styles.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BigCard extends StatelessWidget {
